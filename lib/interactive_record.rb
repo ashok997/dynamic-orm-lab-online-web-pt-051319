c021f2cb6a2a588ec9592ee2e73965732c74152e
ASHOK MAHARJAN <ashok997@gmail.com>
2019-07-28 20:50:19 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -57,10 +57,10 @@ class InteractiveRecord
   end
   
   def self.find_by(hash)
-   name = hash[:name]
+   id = hash[:id]
    #binding.pry
-    sql = "SELECT * FROM #{self.table_name} WHERE name = ?"
-    DB[:conn].execute(sql, name)
+    sql = "SELECT * FROM #{self.table_name} WHERE id = ?"
+    DB[:conn].execute(sql, id)
   end
   
 end
